d135b2e28ab65cebcc0c4a1b32a16770871abdbe
Lukas Trumm <lukas.trumm@gmail.com>
2023-03-10 16:08:48 +0100

    Add nuxt/supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import OpenProps from "open-props"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@vueuse/nuxt"],
+	modules: ["@vueuse/nuxt", "@nuxtjs/supabase"],
 	postcss: {
 		plugins: {
 			"postcss-jit-props": OpenProps,
