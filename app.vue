af8404ee2e90f021f020e6af8d6af28977fd9f9d
Lukas Trumm <lukas.trumm@gmail.com>
2023-05-09 09:59:07 +0200

    Playing with open-props

--- app.vue
+++ app.vue
@@ -3,9 +3,3 @@
 		<NuxtPage />
 	</NuxtLayout>
 </template>
-
-<style>
-@import url("open-props/style");
-@import url("open-props/normalize");
-@import url("open-props/buttons");
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,32 @@
 		<h1 class="heading">Timesheet aggregator</h1>
 		<NuxtLink to="/timesheets"> Timesheets </NuxtLink>
 		<BaseButton>Foo</BaseButton>
+		<p class="one">Some text</p>
+		<p class="two">Some text</p>
+		<p class="three">Some text</p>
+		<p class="four">Some text</p>
 		<UserCard />
 	</div>
 </template>
 
 <style scoped>
 .heading {
-	font-size: var(--red-2);
+	font-size: 2rem;
+}
+
+.one {
+	font-size: var(--font-size-fluid-0);
+}
+
+.two {
+	font-size: var(--font-size-fluid-1);
+}
+
+.three {
+	font-size: var(--font-size-fluid-2);
+}
+
+.four {
+	font-size: var(--font-size-fluid-3);
 }
 </style>
